0177810b880a4b407904b618bf659a2d82df0560
Aldinei <aldinei.franco@unimedprudente.com.br>
2024-02-27 16:43:05 -0300

    signup e login

---
+++ cypress/e2e/signup.cy.js
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker/locale/en'
+
+describe('Sign up', () => {
+  const emailAndress = `${faker.datatype.uuid()}@${Cypress.env(
+    'MAILOSAUR_SERVER_ID'
+  )}.mailosaur.net`
+  const password = Cypress.env('USER_PASSWORD')
+  it('successfully signs up using confirmation code sent via email', () => {
+    cy.fillSignupFormAndSubmit(
+      emailAndress,
+      password
+    )
+
+    cy.contains('h1', 'Your Notes').should('be.visible')
+    cy.contains('a', 'Create a new note').should('be.visible')
+  })
+})
+
